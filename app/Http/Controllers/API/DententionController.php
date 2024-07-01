5ccb040d9425012d58e5ba5b50120cf78a9bd01d
Tarek Hassan <tarek446240@gmail.com>
2024-07-01 18:55:16 +0300

    add dentention Cal

---
+++ app/Http/Controllers/API/DententionController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Containers\Movements;
+use App\Models\Master\Containers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
+
+class DententionController extends Controller
+{
+    //
+    public function getBlnoToBookingNo(Request $request)
+    {
+        $blNo = Movements::where('company_id', $request->company_id)
+            ->where('booking_no', $request->id)
+            ->whereNotNull('bl_no')
+            ->select('bl_no')
+            ->distinct('bl_no')
+            ->pluck('bl_no')
+        ;
+
+        return Response::json([
+            'blNo' => $blNo,
+        ], 200);
+    }
+
+    public function getBlContainers(Request $request)
+    {
+        dd($request->all());
+        $bl_no = $request->bl;
+        $mov = Movements::where('company_id', $request->company_id);
+        if (in_array('all', $bl_no)) {
+            $mov->where('booking_no', $request->booking_no);
+        } else {
+            $mov->whereIn('bl_no', $bl_no);
+        }
+        $mov = $mov->distinct()->pluck('container_id')->toarray();
+        $containers = Containers::select('id', 'code')->whereIn('id', $mov)->get();
+        return $containers;
+    }
+}
